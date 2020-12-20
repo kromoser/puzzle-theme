5745add97313fbaa0be75ff46d4ff23c1a4368f5
kromoser <kevinromoser@gmail.com>
2020-12-20 13:59:44 -0500

    Stub out svg solution for hexes, and adjust 404 template

--- static/js/puzzle.js
+++ static/js/puzzle.js
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const solutionInput = document.querySelector('.solution__input');
   const solutionButton = document.querySelector('.solution__button');
-  const hexes = document.querySelectorAll('.hexagon__letter');
-
+  const cancelButton = document.querySelector('.cancel__button');
+  const hexes = document.querySelectorAll('polygon');
+  const letters = document.querySelectorAll('text');
 
   // Solution handling
   solutionButton.addEventListener('click', function(e) {
@@ -18,13 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
     solutionValue ? window.location = `/${solutionValue}` : alert("You didn\'t enter a solution")
   });
 
+  // Cancel button handling
+  cancelButton.addEventListener('click', function(e) {
+    solutionInput.value = '';
+  })
+
 
   // Hexagon handling
   hexes.forEach(function(hex) {
     hex.addEventListener('click', function(e) {
-      solutionInput.value += hex.textContent;
+      const hexId = hex.id;
+      //console.log(hexId)
+      const hexLetter = document.querySelector(`#text${hexId}`);
+      solutionInput.value += hexLetter.textContent;
+    })
+  });
+
+  // Text handling
+  letters.forEach(function(letter) {
+    letter.addEventListener('click', function(e) {
+      solutionInput.value += letter.textContent;
     })
   })
+
+
 
 
 })
